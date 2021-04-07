d9d2f049ace2c1f6e334950514bc40b5c5d8f928
godinenbicicleta <carballo.bruno@gmail.com>
2021-04-07 14:56:12 -0500

    add ets_for solution

---
+++ elixir/ets_for.ex
@@ -0,0 +1,65 @@
+defmodule Sections do
+  @table :table
+
+  def next_lesson do
+    :ets.update_counter(@table, :lessons, 1, {:lessons, 0})
+  end
+
+  def reset_lessons do
+    :ets.insert(@table, {:lessons, 0})
+  end
+
+  def next_possition do
+    :ets.update_counter(@table, :positions, 1, {:positions, 0})
+  end
+
+  def update(sections) do
+    :ets.new(@table, [:named_table, :set])
+
+    res =
+      for section <- sections do
+        if section["reset_lesson_position"], do: reset_lessons()
+
+        new_lessons =
+          for lesson <- section["lessons"] do
+            Map.put(lesson, "position", next_lesson())
+          end
+
+        Map.merge(section, %{"position" => next_possition(), "lessons" => new_lessons})
+      end
+
+    :ets.delete(@table)
+    res
+  end
+end
+
+sections = [
+  %{
+    "title" => "Getting started",
+    "reset_lesson_position" => false,
+    "lessons" => [
+      %{"name" => "Welcome"},
+      %{"name" => "Installation"}
+    ]
+  },
+  %{
+    "title" => "Basic operator",
+    "reset_lesson_position" => false,
+    "lessons" => [
+      %{"name" => "Addition / Subtraction"},
+      %{"name" => "Multiplication / Division"}
+    ]
+  },
+  %{
+    "title" => "Advanced topics",
+    "reset_lesson_position" => true,
+    "lessons" => [
+      %{"name" => "Mutability"},
+      %{"name" => "Immutability"}
+    ]
+  }
+]
+
+sections
+|> Sections.update()
+|> IO.inspect()
