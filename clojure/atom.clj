b29cbd24a9efaaf24bc7ac385879e08dddd6a0c9
Aleš Roubíček <ales@roubicek.name>
2021-04-08 07:08:44 +0200

    Reformat data

--- clojure/atom.clj
+++ clojure/atom.clj
@@ -1,15 +1,16 @@
-(def data [{:title "Getting started"
-             :reset_lesson_position false
-             :lessons [{:name "Welcome"}
-                       {:name "Installation"}]}
-            {:title "Basic operator"
-             :reset_lesson_position false
-             :lessons [{:name "Addition / Subtraction"}
-                       {:name "Multiplication / Division"}]}
-            {:title "Advanced topics"
-             :reset_lesson_position true
-             :lessons [{:name "Mutability"}
-                       {:name "Immutability"}]}])
+(def data
+   [{:title "Getting started"
+     :reset_lesson_position false
+     :lessons [{:name "Welcome"}
+               {:name "Installation"}]}
+    {:title "Basic operator"
+     :reset_lesson_position false
+     :lessons [{:name "Addition / Subtraction"}
+               {:name "Multiplication / Division"}]}
+    {:title "Advanced topics"
+     :reset_lesson_position true
+     :lessons [{:name "Mutability"}
+               {:name "Immutability"}]}])
 
 (def lesson-position (atom 0))
 (defn lessons [{:keys [lessons reset_lesson_position]}]
