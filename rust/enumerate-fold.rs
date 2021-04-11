56d7b6b32160669ca4f0fded6a379c2edf87b0e3
Daniel Kvasnička <dkvasnicka@users.noreply.github.com>
2021-04-11 22:31:40 +0200

    Rust: sideeffect-free solution using enumerate and fold (#109)
    * Sideeffect-free Rust solution using enumerate & fold
    
    * Cleanup original Rust mutable solution
    
    * fixup! Sideeffect-free Rust solution using enumerate & fold

--- rust/enumerate-fold.rs
+++ rust/enumerate-fold.rs
@@ -1,15 +1,15 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Lesson {
     name: String,
     position: Option<u32>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Section {
     title: String,
     reset_position_title: bool,
     lessons: Vec<Lesson>,
-    position: Option<u32>,
+    position: Option<usize>,
 }
 
 fn main() {
@@ -61,38 +61,35 @@ fn main() {
         },
     ];
 
-    let mut section_position = 0;
-    let mut lesson_position = 0;
+    let formatted_sections =
+        sections
+            .iter()
+            .enumerate()
+            .try_fold(Vec::<Section>::new(), |mut acc, (idx, s)| {
+                let lesson_position = if s.reset_position_title || acc.is_empty() {
+                    1
+                } else {
+                    acc.last()?.lessons.last()?.position? + 1
+                };
 
-    let formatted_sections: Vec<Section> = sections
-        .iter()
-        .map(|s| {
-            if s.reset_position_title == true {
-                lesson_position = 0;
-            }
-
-            section_position += 1;
-
-            Section {
-                lessons: s
-                    .lessons
-                    .iter()
-                    .map(|l| {
-                        lesson_position += 1;
-
-                        Lesson {
-                            position: Some(lesson_position),
+                acc.push(Section {
+                    lessons: s
+                        .lessons
+                        .iter()
+                        .zip(lesson_position..)
+                        .map(|(l, p)| Lesson {
+                            position: Some(p),
                             name: l.name.clone(),
                             ..*l
-                        }
-                    })
-                    .collect(),
-                title: s.title.clone(),
-                position: Some(section_position),
-                ..*s
-            }
-        })
-        .collect();
+                        })
+                        .collect(),
+                    title: s.title.clone(),
+                    position: Some(idx + 1),
+                    ..*s
+                });
 
-    println!("{:?}", formatted_sections);
+                Some(acc)
+            });
+
+    println!("{:?}", formatted_sections.unwrap());
 }
