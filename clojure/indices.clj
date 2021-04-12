ffff3512e242c0ec2b25c032f6475e3643c8e887
Alex Appetiti <alex@appetiti.io>
2021-04-12 20:38:56 +0100

    Corrected clojure/indicies.clj to indices.clj

--- clojure/indices.clj
+++ clojure/indices.clj
@@ -14,14 +14,14 @@
 
 (defn update-sections
   [data]
-  (let [indicies (for [[section-ix section] (map vector (range) data)
+  (let [indices (for [[section-ix section] (map vector (range) data)
                        lesson-ix (-> section :lessons count range)]
                    [section-ix lesson-ix])
-        resets (for [[section-ix lesson-ix] indicies]
+        resets (for [[section-ix lesson-ix] indices]
                  (and (zero? lesson-ix) (get-in data [section-ix :reset-lesson-position])))
         lesson-positions (reductions #(if %2 1 (inc %1)) 1 (rest resets))
         data-with-sections (into [] (map-indexed #(assoc %2 :position (inc %1)) data))]
     (reduce (fn [data [section-ix lesson-ix lesson-position]]
               (assoc-in data [section-ix :lessons lesson-ix :position] lesson-position))
             data-with-sections
-            (map conj indicies lesson-positions))))
+            (map conj indices lesson-positions))))
